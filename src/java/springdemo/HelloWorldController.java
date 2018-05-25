a1ee26f1dd559ed570f2b701922525da546c6aa3
Oleksandr Iaremii <ward.wsp@gmail.com>
2018-05-25 13:25:03 +0200

    add some changes

--- src/java/springdemo/HelloWorldController.java
+++ src/java/springdemo/HelloWorldController.java
@@ -1,7 +1,9 @@
 package springdemo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -13,6 +15,17 @@ public class HelloWorldController {
 
     @RequestMapping("/processForm")
     public String processForm() {
+        return "helloworld";
+    }
+
+    @RequestMapping("/test")
+    //public String addmodel(HttpServletRequest request, Model model) {
+    public String addmodel(@RequestParam("studentName") String name, Model model) {
+        //String name = request.getParameter("studentName");
+        
+        name = name.toUpperCase();
+
+        model.addAttribute("name", name);
         return "helloworld";
     }
 
